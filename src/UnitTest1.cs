c5711918db5750d1389d5d00462920ee1d60e542
Bert Temme <btemme@hotmail.com>
2022-04-04 12:34:40 +0200

    git read version

--- src/UnitTest1.cs
+++ src/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -8,32 +9,38 @@ namespace copc;
 public class Tests
 {
     [Test]
-    public async Task Test1()
+    public async Task ReadCOPC()
     {
-        // sofi stadium
+        // 2.3GB sofi stadium
         var url = "https://s3.amazonaws.com/hobu-lidar/sofi.copc.laz";
 
-        // format https://copc.io/
+        // specs https://copc.io/
         var httpClient = new HttpClient();
-        string lasfHeader= await GetHttpRange(url, httpClient, 0, 3);
+        string lasfHeader= await GetHttpRange(httpClient, url, 0, 3);
         Assert.IsTrue(lasfHeader == "LASF");
 
-        string copcHeader = await GetHttpRange(url, httpClient, 377, 380);
+        string copcHeader = await GetHttpRange(httpClient, url, 377, 380);
         Assert.IsTrue(copcHeader == "copc");
 
+
         var copcVersion = await GetHttpRangeBytes(url, httpClient, 393, 394);
-        
+
         Assert.IsTrue(copcVersion[0] == 1);
         Assert.IsTrue(copcVersion[1] == 0);
+
     }
 
-    private static async Task<string> GetHttpRange(string url, HttpClient httpClient, int start = 0, int end = 0)
+    private static async Task<string> GetHttpRange(HttpClient client, string url, int start, int end)
     {
+        var timer = new Stopwatch();
+        timer.Start();
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         request.Headers.Range = new RangeHeaderValue(start, end);
 
-        var response = await httpClient.SendAsync(request);
+        var response = await client.SendAsync(request);
         var content = await response.Content.ReadAsStringAsync();
+        timer.Stop();
+        Debug.WriteLine($"timer {timer.ElapsedMilliseconds}ms");
 
         return content;
     }
@@ -48,5 +55,4 @@ public class Tests
 
         return content;
     }
-
 }
