eed77f7a7285968abeba07f686183433716a286c
Bert Temme <btemme@hotmail.com>
2022-04-04 12:10:55 +0200

    read version

--- src/UnitTest1.cs
+++ src/UnitTest1.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace src;
+namespace copc;
 
 public class Tests
 {
@@ -13,6 +13,7 @@ public class Tests
         // sofi stadium
         var url = "https://s3.amazonaws.com/hobu-lidar/sofi.copc.laz";
 
+        // format https://copc.io/
         var httpClient = new HttpClient();
         string lasfHeader= await GetHttpRange(url, httpClient, 0, 3);
         Assert.IsTrue(lasfHeader == "LASF");
@@ -20,6 +21,10 @@ public class Tests
         string copcHeader = await GetHttpRange(url, httpClient, 377, 380);
         Assert.IsTrue(copcHeader == "copc");
 
+        var copcVersion = await GetHttpRangeBytes(url, httpClient, 393, 394);
+        
+        Assert.IsTrue(copcVersion[0] == 1);
+        Assert.IsTrue(copcVersion[1] == 0);
     }
 
     private static async Task<string> GetHttpRange(string url, HttpClient httpClient, int start = 0, int end = 0)
@@ -32,4 +37,16 @@ public class Tests
 
         return content;
     }
+
+    private static async Task<byte[]> GetHttpRangeBytes(string url, HttpClient httpClient, int start = 0, int end = 0)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Range = new RangeHeaderValue(start, end);
+
+        var response = await httpClient.SendAsync(request);
+        var content = await response.Content.ReadAsByteArrayAsync();
+
+        return content;
+    }
+
 }
