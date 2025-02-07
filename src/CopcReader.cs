3592530cda3033d28b11c693e8d00260aa3e4f03
Bert Temme <btemme@hotmail.com>
2025-02-07 23:42:52 +0100

    remove BinaryFileProcessor

--- src/CopcReader.cs
+++ src/CopcReader.cs
@@ -5,26 +5,26 @@ public static class CopcReader
 {
     public static async Task<Copc> Read(string file)
     {
-        var processor = new BinaryFileProcessor(new LocalBinaryFileReader(file));
+        var processor = new LocalBinaryFileReader(file);
         var copc = await Read(processor);
         return copc;
     }
 
     public static async Task<Copc> Read(HttpClient client, string url)
     {
-        var processor = new BinaryFileProcessor(new HttpBinaryFileReader(client, url));
+        var processor = new HttpBinaryFileReader(client, url);
         var copc = await Read(processor);
         return copc;
     }
 
-    private static async Task<Copc> Read(BinaryFileProcessor processor)
+    private static async Task<Copc> Read(BinaryFileReader binaryFileReader)
     {
-        var headerBytes = await processor.Read(0, 589);
+        var headerBytes = await binaryFileReader.Read(0, 589);
         var reader = new BinaryReader(new MemoryStream(headerBytes));
         var copc = Read(reader);
         var header = copc.Header;
         var vlrCount = (int)header.VlrCount;
-        var vlrs = await VlrReader.GetVlrs(processor, vlrCount);
+        var vlrs = await VlrReader.GetVlrs(binaryFileReader, vlrCount);
         copc.Vlrs.AddRange(vlrs);
         return copc;
     }
--- src/VlrReader.cs
+++ src/VlrReader.cs
@@ -3,14 +3,14 @@
 namespace copc;
 public static class VlrReader
 {
-    public static async Task<List<VlrInfo>> GetVlrs(BinaryFileProcessor processor, int vlrCount)
+    public static async Task<List<VlrInfo>> GetVlrs(BinaryFileReader binaryFileReader, int vlrCount)
     {
         var vlrs = new List<VlrInfo>();
         var start = 589;
         for (var i = 0; i < vlrCount - 1; i++)
         {
             var end = start + (i + 1) * 54;
-            var vlr = await GetVlr(processor, start, end);
+            var vlr = await GetVlr(binaryFileReader, start, end);
             vlrs.Add(vlr);
 
             start = end;
@@ -24,7 +24,7 @@ public static class VlrReader
         return vlrs;
     }
 
-    private static async Task<VlrInfo> GetVlr(BinaryFileProcessor processor, int start, int end)
+    private static async Task<VlrInfo> GetVlr(BinaryFileReader processor, int start, int end)
     {
         var headerBytes1 = await processor.Read(start, end);
         var reader = new BinaryReader(new MemoryStream(headerBytes1));
